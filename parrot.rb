e4d2782cc2aba6c4b4194d9c62a04b1fdfa51cb9
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-13 13:27:25 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  puts "Squawk!"
+end
+
+def parrot(phrase = "Squawk!")
+  puts phrase
+  return phrase
+end
